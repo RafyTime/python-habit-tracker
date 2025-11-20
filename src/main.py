644b0b007694794a1653c4a5490959929aeeff0d
Leonardo Plata <lplata1903@gmail.com>
2025-11-20 15:35:01 +0100

    🩹😅 Fix silly AttributeError: 'Settings' object has no attribute 'DEFAULT_MESSAGE'

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from rich import print
 from typer import Typer
-from src.core.config import settings
 
+from src.core.config import settings
 
 app = Typer()
 
@@ -10,7 +10,6 @@ app = Typer()
 def run():
     print(f'✨ [cyan]Hello from {settings.PROJECT_NAME}![/cyan] 🚀')
     print(f'Version: {settings.PROJECT_VERSION}')
-    print(f'Default Message: {settings.DEFAULT_MESSAGE}')
 
 
 if __name__ == '__main__':
