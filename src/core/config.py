e48a53a3ccdf0fd87e26225bedb8345684b607aa
Leonardo Plata <lplata1903@gmail.com>
2025-11-26 11:51:20 +0100

    🔧 Setup SQLModel ORM & tweak .env configs

--- src/core/config.py
+++ src/core/config.py
@@ -4,11 +4,15 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
+    # Project Details
     PROJECT_NAME: str = 'Project Name'
     PROJECT_VERSION: str = '0.0.0'
     PROJECT_DESCRIPTION: str = 'Add your description here'
+    # Environment
     ENVIRONMENT: Literal['dev', 'prod'] = 'dev'
     DEBUG: bool = False
+    # Database
+    DATABASE_URL: str = 'sqlite:///db.sqlite'
 
 
 app_settings = Settings()
---
+++ src/core/db.py
@@ -0,0 +1,16 @@
+from collections.abc import Generator
+
+from sqlmodel import Session, SQLModel, create_engine
+
+from .config import app_settings
+
+engine = create_engine(app_settings.DATABASE_URL, echo=app_settings.DEBUG)
+
+
+def init_db() -> None:
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session() -> Generator[Session]:
+    with Session(engine) as session:
+        yield session
