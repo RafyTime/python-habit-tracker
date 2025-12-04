fdb7287b7c5d55a9048ea302e21315fce2a47543
Leonardo Plata <lplata1903@gmail.com>
2025-12-04 12:18:53 +0100

    🔧 Fix app_settings not reading the .env file properly

--- src/core/config.py
+++ src/core/config.py
@@ -1,9 +1,17 @@
 from typing import Literal
 
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file='.env',
+        env_file_encoding='utf-8',
+        case_sensitive=True,
+        env_ignore_empty=True,
+        extra='ignore',
+    )
+
     # Project Details
     PROJECT_NAME: str = 'Project Name'
     PROJECT_VERSION: str = '0.0.0'
